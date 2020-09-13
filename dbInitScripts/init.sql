6e61aa369d0df19142018da4c17a8ff2c18c7a90
Ilmari Tarpila <leo.tarpila@aalto.fi>
2020-09-13 19:37:09 +0300

    Add db init scripts

---
+++ dbInitScripts/init.sql
@@ -0,0 +1,26 @@
+CREATE TABLE Events (
+    id SERIAL PRIMARY KEY, 
+    title TEXT, 
+    owner INT, 
+    date DATE
+    );
+CREATE TABLE Users (
+    id SERIAL PRIMARY KEY, 
+    handle TEXT, 
+    name TEXT
+    );
+CREATE TABLE DoorEvents (
+    id SERIAL PRIMARY KEY, 
+    createdAt TIMESTAMP, 
+    eventId INT, 
+    userId INT, 
+    inOrOut BOOLEAN
+    );
+CREATE TABLE Admins (
+    id SERIAL PRIMARY KEY, 
+    createdAt TIMESTAMP, 
+    eventId INT, 
+    userId INT,
+    approvingAdminId INT,
+    AdminStatus BOOLEAN
+);
