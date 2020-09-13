6748fbbe11898277fd23f9ce51dbab47c4f69e4b
Visa Pollari <visa.pollari@gmail.com>
2020-09-13 22:35:25 +0300

    add possu

--- src/bot.py
+++ src/bot.py
@@ -1,11 +1,17 @@
 import os
+
+import psycopg2
 from telegram.ext import Updater
 import logging
 
 import handlers
-
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
+
+# conn = psycopg2.connect("dbname=test user=postgres")
+# cur = conn.cursor()
+# cur.execute("SELECT * FROM my_data");
+# records = cur.fetchall()
 
 updater = Updater(
     token=os.environ['BOT_TOKEN'], use_context=True)
